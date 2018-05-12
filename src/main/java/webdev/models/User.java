02658a46e0059063d72a9e587c142016bd316a77
Ishan Patel <i@patelishan.com>
2018-05-12 12:06:20 -0400

    Added User Model

---
+++ src/main/java/webdev/models/User.java
@@ -0,0 +1,53 @@
+package webdev.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Getter
+    @Setter
+    private String username;
+
+    @Getter
+    @Setter
+    private String password;
+
+    @Getter
+    @Setter
+    private String firstName;
+
+    @Getter
+    @Setter
+    private String lastName;
+
+    @Getter
+    @Setter
+    private String phone;
+
+    @Getter
+    @Setter
+    private String email;
+
+    @Getter
+    @Setter
+    private String role;
+
+    @Getter
+    @Setter
+    private Date dateOfBirth;
+}
--- src/test/java/webdev/CoursemanagementApplicationTests.java
+++ src/test/java/webdev/CoursemanagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.coursemanagement;
+package webdev;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
