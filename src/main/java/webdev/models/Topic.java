32bcc366d1cc18b4ffdbcd40e2224d95418d6bef
Ishan Patel <i@patelishan.com>
2018-05-22 22:00:38 -0400

    Created Topic model

--- src/main/java/webdev/models/Topic.java
+++ src/main/java/webdev/models/Topic.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 public class Topic {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     @Setter
     private int id;
