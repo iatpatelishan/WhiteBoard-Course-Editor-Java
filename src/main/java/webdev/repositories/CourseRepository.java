d1ce698936754c940651ad493ce91b3040e891ab
Ishan Patel <i@patelishan.com>
2018-06-05 04:05:58 -0400

    Created Assignment Repository

--- src/main/java/webdev/repositories/CourseRepository.java
+++ src/main/java/webdev/repositories/CourseRepository.java
@@ -3,6 +3,6 @@ package webdev.repositories;
 import org.springframework.data.repository.CrudRepository;
 import webdev.models.Course;
 
-    public interface CourseRepository extends CrudRepository<Course, Integer> {
+public interface CourseRepository extends CrudRepository<Course, Integer> {
 
 }
