6f53c247a89a85678bbc75f3dcf00aea23d86ce0
Ishan Patel <i@patelishan.com>
2018-06-05 04:44:22 -0400

    Created Exam modal file

---
+++ src/main/java/webdev/models/Exam.java
@@ -0,0 +1,18 @@
+package webdev.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Exam extends Widget {
+
+    
+
+    public Exam() {
+        super("Exam");
+    }
+}
