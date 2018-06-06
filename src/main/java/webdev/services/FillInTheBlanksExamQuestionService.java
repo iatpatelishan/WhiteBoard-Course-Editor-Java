971dc79b372ed6becf18e152fd443989a8adeb5e
Ishan Patel <i@patelishan.com>
2018-06-06 03:55:20 -0400

    Created FillInTheBlanksExamQuestion Service

--- src/main/java/webdev/services/FillInTheBlanksExamQuestionService.java
+++ src/main/java/webdev/services/FillInTheBlanksExamQuestionService.java
@@ -3,43 +3,45 @@ package webdev.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import webdev.models.Exam;
+import webdev.models.exam.FillInTheBlanksExamQuestion;
 import webdev.models.exam.MultipleChoiceExamQuestion;
 import webdev.repositories.ExamRepository;
+import webdev.repositories.exam.FillInTheBlanksExamQuestionRepository;
 import webdev.repositories.exam.MultipleChoiceExamQuestionRepository;
 
 import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-public class MultipleChoiceExamQuestionService {
+public class FillInTheBlanksExamQuestionService {
 
     @Autowired
     ExamRepository examRepository;
 
     @Autowired
-    MultipleChoiceExamQuestionRepository multipleChoiceExamQuestionRepository;
+    FillInTheBlanksExamQuestionRepository fillInTheBlanksExamQuestionRepository;
 
-    @GetMapping("/api/question/choice/{id}")
-    public Optional<MultipleChoiceExamQuestion> findQuestionById(@PathVariable("id") int id) {
-        return multipleChoiceExamQuestionRepository.findById(id);
+    @GetMapping("/api/question/blanks/{id}")
+    public Optional<FillInTheBlanksExamQuestion> findQuestionById(@PathVariable("id") int id) {
+        return fillInTheBlanksExamQuestionRepository.findById(id);
     }
 
     @PostMapping("/api/exam/{eid}/choice")
-    public MultipleChoiceExamQuestion createQuestion(@PathVariable("eid") int eid, @RequestBody MultipleChoiceExamQuestion question) {
+    public FillInTheBlanksExamQuestion createQuestion(@PathVariable("eid") int eid, @RequestBody FillInTheBlanksExamQuestion question) {
         Optional<Exam> data = examRepository.findById(eid);
         if (data.isPresent()) {
             question.setId(null);
             question.setExam(data.get());
-            return multipleChoiceExamQuestionRepository.save(question);
+            return fillInTheBlanksExamQuestionRepository.save(question);
         }
         return null;
     }
 
-    @PutMapping("/api/question/choice/{id}")
-    public MultipleChoiceExamQuestion updateQuestion(@PathVariable("id") int id, @RequestBody MultipleChoiceExamQuestion newQuestion) {
-        Optional<MultipleChoiceExamQuestion> data = multipleChoiceExamQuestionRepository.findById(id);
+    @PutMapping("/api/question/blanks/{id}")
+    public FillInTheBlanksExamQuestion updateQuestion(@PathVariable("id") int id, @RequestBody FillInTheBlanksExamQuestion newQuestion) {
+        Optional<FillInTheBlanksExamQuestion> data = fillInTheBlanksExamQuestionRepository.findById(id);
         if (data.isPresent()) {
-            MultipleChoiceExamQuestion question = data.get();
+            FillInTheBlanksExamQuestion question = data.get();
             if(newQuestion.getTitle()!=null){
                 question.setTitle(newQuestion.getTitle());
             }
@@ -59,13 +61,11 @@ public class MultipleChoiceExamQuestionService {
                 question.setExam(newQuestion.getExam());
             }
 
-            if(newQuestion.getOptions()!=null){
-                question.setOptions(newQuestion.getOptions());
+            if(newQuestion.getVariables()!=null){
+                question.setVariables(newQuestion.getVariables());
             }
-            if(newQuestion.getAnswer()!=null){
-                question.setAnswer(newQuestion.getAnswer());
-            }
-            return multipleChoiceExamQuestionRepository.save(question);
+
+            return fillInTheBlanksExamQuestionRepository.save(question);
         }
         return null;
     }
