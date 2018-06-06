7dffd249b61fe4f75978a4b09fbed4e326ebe673
Ishan Patel <i@patelishan.com>
2018-06-06 01:28:21 -0400

    Bug Fix in Modal

--- src/main/java/webdev/models/exam/BaseExamQuestion.java
+++ src/main/java/webdev/models/exam/BaseExamQuestion.java
@@ -5,18 +5,17 @@ import lombok.Getter;
 import lombok.Setter;
 import webdev.models.Exam;
 
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
 public class BaseExamQuestion {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Getter
     @Setter
     private int id;
-
-    @Getter
-    @Setter
-    private String description;
 
     @Getter
     @Setter
@@ -28,11 +27,15 @@ public class BaseExamQuestion {
 
     @Getter
     @Setter
+    private String description;
+
+    @Getter
+    @Setter
     private String instructions;
 
     @Getter
     @Setter
-    private String typeofQuestion;
+    private String questionType;
 
     @ManyToOne
     @JsonIgnore
